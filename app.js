c6f63159f8b434909fec44e0dd2fcccef23d049c
VictorGalvez1203 <victorgalvez0312@gmail.com>
2025-08-15 10:12:55 -0400

    Subiendo proyecto completo a main

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = 3008;
+
+app.get('/', (req, res) => {
+    res.send('Hola Mundo desde Express ITLA, por victor Galvez/2023-1836 desplegado en Render');
+});
+
+if (require.main === module) {
+    app.listen(PORT, () => {
+        console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
+    });
+}
+
+module.exports = app;
+
