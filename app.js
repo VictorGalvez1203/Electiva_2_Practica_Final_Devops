7a577e36d5bde10a06930bda4c611fb2540f3784
Victor Eulogio Galvez Faña <victorgalvez0312@gmail.com>
2025-08-15 10:33:14 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3008;
 
 app.get('/', (req, res) => {
-    res.send('Hola Mundo desde Express ITLA, por victor Galvez/2023-1836 desplegado en Render');
+    res.send('Hola Mundo desde Express ITLA, por victor Galvez/2023-1836 desplegado en Render, Practica Final Devops');
 });
 
 if (require.main === module) {
